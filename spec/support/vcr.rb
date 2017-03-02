5f4f3da4cd9ee141823ee48ccbd9f611d240e549
Zhuo-Fei Hui <zhfhui+github@gmail.com>
2017-03-02 16:03:15 +0100

    Add vcr gem and config

---
+++ spec/support/vcr.rb
@@ -0,0 +1,10 @@
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/support/vcr_cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = {
+    record: :new_episodes
+  }
+  config.configure_rspec_metadata!
+end
